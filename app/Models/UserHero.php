192e676e0ed196c7c9173766283176d50faf76e1
Vasiliy Nikiforov <kaktusv6@gmail.com>
2020-12-30 23:29:21 +1000

    Refactor code to model heroes

--- app/Models/UserHero.php
+++ app/Models/UserHero.php
@@ -2,11 +2,13 @@
 
 namespace App\Models;
 
+use App\Formatters\AttributeValueFormatter;
 use App\Formatters\CharacteristicValueFormatter;
 use Backpack\CRUD\app\Models\Traits\CrudTrait;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class UserHero extends Model
 {
@@ -20,24 +22,12 @@ class UserHero extends Model
         'user_Id',
     ];
 
-    public function game() : BelongsTo
-    {
-        return $this->belongsTo(Game::class);
-    }
+    public function game() : BelongsTo { return $this->belongsTo(Game::class); }
+    public function user() : BelongsTo { return $this->belongsTo(User::class); }
 
-    public function user() : BelongsTo
-    {
-        return $this->belongsTo(User::class);
-    }
+    public function characteristicValues() : BelongsToMany { return $this->belongsToMany(Characteristic::class, 'user_heroes_characteristics', 'hero_id', null)->withPivot(['value'])->where('game_id', $this->game_id); }
+    public function attributeValues() : HasMany { return $this->hasMany(AttributeValue::class, 'hero_id'); }
 
-    public function characteristicValues() : BelongsToMany
-    {
-        return $this->belongsToMany(Characteristic::class, 'user_heroes_characteristics', 'hero_id', null)->withPivot(['value'])->where('game_id', $this->game_id);
-    }
-
-    public function getCharacteristicValuesAttribute() : array
-    {
-        return app(CharacteristicValueFormatter::class)->formatList($this->characteristicValues()->get());
-    }
+    public function getCharacteristicValuesAttribute() : array { return app(CharacteristicValueFormatter::class)->formatList($this->characteristicValues()->get()); }
     public function getAttributeValuesAttribute() : array { return app(AttributeValueFormatter::class)->formatList($this->attributeValues()->get()); }
 }
