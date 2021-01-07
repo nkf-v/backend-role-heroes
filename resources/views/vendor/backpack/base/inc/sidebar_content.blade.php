1fdb19107ce1c5f20e23a8243fac065cd670c7d1
Vasiliy Nikiforov <kaktusv6@gmail.com>
2021-01-07 21:58:56 +1000

    Revert "Install baclpack"
    This reverts commit baf0a369e3895628ee8ec3508ba95192c7c30e04.
    
    # Conflicts:
    #	composer.json
    #	composer.lock
    #	resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
    #	routes/backpack/custom.php

--- resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++
@@ -1,9 +0,0 @@
-<!-- This file is used to store sidebar items, starting with Backpack\Base 0.9.0 -->
-<li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="la la-home nav-icon"></i> {{ trans('backpack::base.dashboard') }}</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('user') }}'><i class='nav-icon la la-question'></i> Users</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('hero') }}'><i class='nav-icon la la-question'></i> Heroes</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('game') }}'><i class='nav-icon la la-question'></i> Games</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('characteristic') }}'><i class='nav-icon la la-question'></i> Characteristics</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('attributecategory') }}'><i class='nav-icon la la-question'></i> Attribute categories</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('attribute') }}'><i class='nav-icon la la-question'></i> Attributes</a></li>
-
