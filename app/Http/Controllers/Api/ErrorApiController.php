88d2bdfd19734c1698f6fb24e865a368fb4633ac
Vasiliy Nikiforov <kaktusv6@gmail.com>
2021-09-27 21:23:57 +1000

    Update erorr route

--- app/Http/Controllers/Api/ErrorApiController.php
+++ app/Http/Controllers/Api/ErrorApiController.php
@@ -3,11 +3,12 @@
 namespace App\Http\Controllers\Api;
 
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Response;
 
 class ErrorApiController extends ApiController
 {
     public function error() : JsonResponse
     {
-        return $this->respondContent(['error']);
+        return $this->respondContent(['error'], Response::HTTP_INTERNAL_SERVER_ERROR);
     }
 }
